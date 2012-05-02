8486280068c244f8a2db14ea040147de96d13234
Filipe Fernandes <ocefpaf@gmail.com>
2012-05-02 18:44:35 -0400

    Renamed geostrophic48.py to geostrophic_48.py.

---
+++ gsw/gibbs/geostrophic_48.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import division
+
+import numpy as np
+
+__all__ = [
+           #'geo_strf_dyn_height',  TODO
+           #'geo_strf_dyn_height_pc',  TODO
+           #'geo_strf_isopycnal',  TODO
+           #'geof_str_isopycnal_pc',  TODO
+           #'geo_strf_Montgomery',  TODO
+           #'geo_strf_Cunningham'  TODO
+           ]
