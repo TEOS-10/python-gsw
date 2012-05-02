760b5ccf2238b83e8ca55aa6bf2b93ff2dd74cbd
Filipe Fernandes <ocefpaf@gmail.com>
2012-05-02 17:24:06 -0400

    Added __all__ list with TODO marks.

--- gsw/gibbs/density_enthalpy_48.py
+++ gsw/gibbs/density_enthalpy_48.py
@@ -1,1 +1,27 @@
 # -*- coding: utf-8 -*-
+
+from __future__ import division
+
+import numpy as np
+
+__all__ = [
+           #'rho_CT',  TODO
+           #'alpha_CT',  TODO
+           #'beta_CT',  TODO
+           #'rho_alpha_beta_CT',  TODO
+           #'specvol_CT',  TODO
+           #'specvol_anom_CT',  TODO
+           #'sigma0_CT',  TODO
+           #'sigma1_CT',  TODO
+           #'sigma2_CT',  TODO
+           #'sigma3_CT',  TODO
+           #'sigma4_CT',  TODO
+           #'sound_speed_CT',  TODO
+           #'internal_energy_CT',  TODO
+           #'enthalpy_CT',  TODO
+           #'enthalpy_diff_CT',  TODO
+           #'dynamic_enthalpy_CT',  TODO
+           #'SA_from_rho_CT',  TODO
+           #'CT_from_rho',  TODO
+           #'CT_maxdensity', TODO
+           ]
