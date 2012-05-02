6b17d73d84090feab96cfa093c419afe5bd047b5
Filipe Fernandes <ocefpaf@gmail.com>
2012-05-02 19:05:09 -0400

    Added __all__ list with TODO marks.

--- gsw/gibbs/water_column_48.py
+++ gsw/gibbs/water_column_48.py
@@ -1,1 +1,11 @@
 # -*- coding: utf-8 -*-
+
+from __future__ import division
+
+import numpy as np
+
+__all__ = [
+           #'Nsquared',  TODO
+           #'Turner_Rsubrho',  TODO
+           #'IPV_vs_fNsquared_ratio'  TODO
+           ]
