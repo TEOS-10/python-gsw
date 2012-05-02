b8e596ce05933697093d9cdee71879fdeafe6f1e
Filipe Fernandes <ocefpaf@gmail.com>
2012-05-02 19:04:08 -0400

    Added __all__ list with TODO marks.

--- gsw/gibbs/neutral_nonlinear_48.py
+++ gsw/gibbs/neutral_nonlinear_48.py
@@ -1,1 +1,13 @@
 # -*- coding: utf-8 -*-
+
+from __future__ import division
+
+import numpy as np
+
+__all__ = [
+           #'cabbeling',  TODO
+           #'thermobaric',  TODO
+           #'isopycnal_slope_ratio',  TODO
+           #'isopycnal_vs_ntp_CT_ratio',  TODO
+           #'ntp_pt_vs_CT_ratio'  TODO
+           ]
