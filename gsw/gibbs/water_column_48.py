be62d1dcb1c466475577b4a8924389fbeeb28434
Filipe Fernandes <ocefpaf@gmail.com>
2012-05-16 14:50:54 -0400

    Implemented Turner_Rsubrho.

--- gsw/gibbs/water_column_48.py
+++ gsw/gibbs/water_column_48.py
@@ -8,10 +8,11 @@ from constants import db2Pascal
 from earth import grav
 from density_enthalpy_48 import rho
 from gsw.utilities import match_args_return
+from density_enthalpy_48 import rho_alpha_beta
 
 __all__ = [
            'Nsquared',
-           #'Turner_Rsubrho',  TODO
+           'Turner_Rsubrho',
            #'IPV_vs_fNsquared_ratio'  TODO
            ]
 
@@ -41,7 +42,7 @@ def Nsquared(SA, CT, p, lat=None):
     N2 : array_like
          Brunt-Väisälä frequency squared [1 s :math:`-2`]
     p_mid : array_like
-            Mid pressure between p grid
+            Mid pressure between p grid [dbar]
 
     See Also
     --------
@@ -80,7 +81,7 @@ def Nsquared(SA, CT, p, lat=None):
     Princeton, NJ: Princeton University Press, 518 pp + xxxiv.
 
     Modifications:
-    2011-04-22. Trevor McDougall & Paul Barker
+    2011-03-22. Trevor McDougall & Paul Barker
     """
 
     if lat != None:
@@ -101,3 +102,107 @@ def Nsquared(SA, CT, p, lat=None):
     N2 = grav_local ** 2 * drho / (db2Pascal * dp)
 
     return N2, p_mid
+
+
+@match_args_return
+def Turner_Rsubrho(SA, CT, p):
+    r"""Calculates the Turner angle and the Rsubrho as a function of pressure
+    down a vertical water column.  These quantities express the relative
+    contributions of the vertical gradients of Conservative Temperature and
+    Absolute Salinity to the vertical stability (the square of the
+    Brunt-Väisälä Frequency squared, N^2).  `Tu` and `Rsubrho` are evaluated at
+    the mid pressure between the individual data points in the vertical.  This
+    function uses computationally-efficient 48-term expression for density in
+    terms of SA, CT and p (McDougall et al., 2011).  Note that in the
+    double-diffusive literature, papers concerned with the "diffusive" form of
+    double-diffusive convection often define the stability ratio as the
+    reciprocal of what is defined here as the stability ratio.
+
+    Parameters
+    ----------
+    SA : array_like
+         Absolute Salinity  [g/kg]
+    CT : array_like
+         Conservative Temperature [deg C]
+    p : array_like
+        sea pressure  [dbar]
+
+    Returns
+    -------
+    Tu : array_like
+         Turner angle, on the same (M-1)xN grid as p_mid. [degrees of rotation]
+    Rsubrho : array_like
+              Stability Ratio, on the same (M-1)xN grid as p_mid. [unitless]
+    p_mid : array_like
+            Mid pressure between p grid [dbar]
+
+    See Also
+    --------
+    TODO
+
+    Notes
+    -----
+    The 48-term equation has been fitted in a restricted range of parameter
+    space, and is most accurate inside the "oceanographic funnel" described in
+    McDougall et al. (2011).  The GSW library function "infunnel(SA, CT, p)" is
+    available to be used if one wants to test if some of one's data lies
+    outside this "funnel".
+
+    Examples
+    --------
+    TODO
+
+    References
+    ----------
+    .. [1] IOC, SCOR and IAPSO, 2010: The international thermodynamic equation
+    of seawater - 2010: Calculation and use of thermodynamic properties.
+    Intergovernmental Oceanographic Commission, Manuals and Guides No. 56,
+    UNESCO (English), 196 pp. See Eqns. (3.15.1) and (3.16.1).
+
+    ..[2] McDougall T.J., P.M. Barker, R. Feistel and D.R. Jackett, 2011:  A
+    computationally efficient 48-term expression for the density of
+    seawater in terms of Conservative Temperature, and related properties
+    of seawater.
+
+    Modifications:
+    2011-03-26. Trevor McDougall & Paul Barker
+    """
+
+    if SA.ndim == 1:
+        raise ValueError('There must be at least 2 columns.')
+
+    SA.clip(0, np.inf)
+
+    SA, CT, p = np.broadcast_arrays(SA, CT, p)
+
+    p_mid = 0.5 * (p[0:-1, ...] + p[1:, ...])
+    SA_mid = 0.5 * (SA[0:-1, ...] + SA[1:, ...])
+    CT_mid = 0.5 * (CT[0:-1, ...] + CT[1:, ...])
+
+    dSA = SA[0:-1, ...] - SA[1:, ...]
+    dCT = CT[0:-1, ...] - CT[1:, ...]
+
+    [dummy, alpha, beta] = rho_alpha_beta(SA_mid, CT_mid, p_mid)
+
+    """This function evaluates Tu and Rsubrho using the computationally
+    efficient 48-term expression for density in terms of SA, CT and p. If one
+    wanted to compute Tu and Rsubrho using the full TEOS-10 Gibbs function
+    expression for density, the following lines of code would do that.
+
+    pt_mid = pt_from_CT(SA_mid, CT_mid)
+    pr0 = np.zeros_like(SA_mid)
+    t_mid = pt_from_t(SA_mid, pt_mid, pr0, p_mid)
+    beta = beta_const_CT_t_exact(SA_mid, t_mid, p_mid)
+    alpha = alpha_wrt_CT_t_exact(SA_mid, t_mid, p_mid)
+    """
+
+    Tu = np.arctan2((alpha * dCT + beta * dSA), (alpha * dCT - beta * dSA))
+
+    Tu = Tu * (180 / np.pi)
+
+    Rsubrho = np.zeros_like(dSA) + np.NaN
+
+    Inz = dSA != 0
+    Rsubrho[Inz] = (alpha[Inz] * dCT[Inz]) / (beta[Inz] * dSA[Inz])
+
+    return Tu, Rsubrho, p_mid
