332fc8f3370a81dc23f4ed7da88da10ff139936f
Filipe Fernandes <ocefpaf@gmail.com>
2012-05-18 16:36:56 -0400

    Docstring fixes.

--- gsw/gibbs/constants.py
+++ gsw/gibbs/constants.py
@@ -5,7 +5,7 @@
 from __future__ import division
 
 C3515 = 42.9140
-"""Conductivity of 42.914 [mmho cm :sup:`-1` == mS cm :sup:`-1`] at Salinity
+r"""Conductivity of 42.914 [mmho cm :sup:`-1` == mS cm :sup:`-1`] at Salinity
 35 psu, Temperature 15 :math:`^\\circ` C [ITPS 68] and Pressure 0 db.
 
 References
@@ -18,10 +18,10 @@ See also (Culkin and Smith, 1980; UNESCO, 1983.)
 """
 
 earth_radius = 6371000.
-"""Mean radius of earth  A.E. Gill."""
+r"""Mean radius of earth  A.E. Gill."""
 
 OMEGA = 7.292115e-5
-""":math:`\\Omega = \\frac{2\\pi}{\\textrm{sidereal day}}` =
+r""":math:`\\Omega = \\frac{2\\pi}{\\textrm{sidereal day}}` =
                                                  7.292e-5.radians sec :sup:`-1`
 
 1 sidereal day = 23.9344696 hours
@@ -37,11 +37,11 @@ Estimates of the Parameters of Common Relevance to Astronomy, Geodesy, and
 Geodynamics. Journal of Geodesy, 77, pp. 724-797."""
 
 gdef = 9.8
-"""Acceleration of gravity [m s :sup:`2`] used by sw.swvel and bfrq without lat
+r"""Acceleration of gravity [m s :sup:`2`] used by sw.swvel and bfrq without lat
 info."""
 
 DEG2NM, NM2KM = 60., 1.8520
-"""1 nm = 1.8520 km
+r"""1 nm = 1.8520 km
 
 Used by sw.dist() to convert nautical miles to kilometers.
 
@@ -51,30 +51,29 @@ References
 Oceanography Pergamon Press Sydney. ISBN 0-08-028728-X. page: 303."""
 
 T0 = Kelvin = 273.15
-"""The Celcius zero point; 273.15 K.  That is T = t + T0 where T is the
+r"""The Celcius zero point; 273.15 K.  That is T = t + T0 where T is the
 Absolute Temperature (in degrees K) and t is temperature in degrees C."""
 
 db2Pascal = 1e4
-"""Decibar to pascal."""
+r"""Decibar to pascal."""
 
-# Only used by GSW
 gamma = 2.26e-7
-"""Gamma (A.E. Gill)."""
+r"""Gamma (A.E. Gill)."""
 
 M_S = 0.0314038218
-"""Mole-weighted average atomic weight of the elements of
+r"""Mole-weighted average atomic weight of the elements of
 Reference-Composition sea salt, in units of kg mol :sup:`-1`. Strictly
 speaking, the formula below applies only to seawater of Reference Composition.
 If molality is required to an accuracy of better than 0.1% we suggest you
 contact the authors for further guidance."""
 
 cp0 = 3991.86795711963
-"""The "specific heat" for use with Conservative Temperature. cp0 is the ratio
+r"""The "specific heat" for use with Conservative Temperature. cp0 is the ratio
 of potential enthalpy to Conservative Temperature.
 See Eqn. (3.3.3) and Table D.5 from IOC et al. (2010)."""
 
 SSO = 35.16504
-"""SSO is the Standard Ocean Reference Salinity (35.16504 g/kg.)
+r"""SSO is the Standard Ocean Reference Salinity (35.16504 g/kg.)
 
 SSO is the best estimate of the Absolute Salinity of Standard Seawater
 when the seawater sample has a Practical Salinity, SP, of 35
@@ -94,17 +93,17 @@ Reference-Composition Salinity Scale, Deep-Sea Res. I, 55, 50-72.
 See Table 4 and section 5."""
 
 sfac = 0.0248826675584615
-"""sfac = 1 / (40 * (SSO / 35))."""
+r"""sfac = 1 / (40. * (SSO / 35.))"""
 
 R = 8.314472
-"""The molar gas constant = 8.314472 m :sup:`2` kg s:sup:`-21 K :sup:`-1`
+r"""The molar gas constant = 8.314472 m :sup:`2` kg s:sup:`-21 K :sup:`-1`
 mol :sup:`-1`."""
 
 r1 = 0.35
 """ TODO """
 
 uPS = SSO / 35
-"""The unit conversion factor for salinities (35.16504/35) g/kg (Millero et
+r"""The unit conversion factor for salinities (35.16504/35) g/kg (Millero et
 al., 2008). Reference Salinity SR is uPS times Practical Salinity SP.
 
 Ratio, unit conversion factor for salinities [g kg :sup:`-1`]
@@ -118,10 +117,10 @@ section 6, Eqn.(6.1)."""
 
 
 P0 = 101325
-"""Absolute Pressure of one standard atmosphere in Pa, 101325 Pa."""
+r"""Absolute Pressure of one standard atmosphere in Pa, 101325 Pa."""
 
 SonCl = 1.80655
-"""The ratio of Practical Salinity, SP, to Chlorinity, 1.80655 kg/g for
+r"""The ratio of Practical Salinity, SP, to Chlorinity, 1.80655 kg/g for
 Reference Seawater (Millero et al., 2008). This is the ratio that was used by
 the JPOTS committee in their construction of the 1978 Practical Salinity Scale
 (PSS-78) to convert between the laboratory measurements of seawater samples
@@ -135,7 +134,7 @@ Reference-Composition Salinity Scale, Deep-Sea Res. I, 55, 50-72. See section
 5 below Eqn. (5.5)."""
 
 atomic_weight = 31.4038218
-"""This function returns the mole-weighted atomic weight of sea salt of
+r"""This function returns the mole-weighted atomic weight of sea salt of
 Reference Composition, which is 31.4038218 g/mol.  This has been
 defined as part of the Reference-Composition Salinity Scale of 2008
 (Millero et al., 2008).
@@ -153,7 +152,7 @@ Reference-Composition Salinity Scale, Deep-Sea Res. I, 55, 50-72.
 See Eqn. (5.3)"""
 
 valence_factor = 1.2452898
-"""This function returns the valence factor of sea salt of Reference
+r"""This function returns the valence factor of sea salt of Reference
 Composition, 1.2452898.  This valence factor is exact, and follows from
 the definition of the Reference-Composition Salinity Scale 2008 of
 Millero et al. (2008).  The valence factor is the mole-weighted square
