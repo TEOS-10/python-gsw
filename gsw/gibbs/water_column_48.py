0dc3dea5b25e9be4dc8141a4e901d31bc787ca8e
Filipe Fernandes <ocefpaf@gmail.com>
2012-05-19 19:56:42 -0400

    Added IPV_vs_fNsquared_ratio. The water_column_48.py is now fully implemented. However, there is an "invisible" issue with `@match_args_return` it is returning a ndarray instead of a tuple.

--- gsw/gibbs/water_column_48.py
+++ gsw/gibbs/water_column_48.py
@@ -13,11 +13,15 @@ from density_enthalpy_48 import rho_alpha_beta
 __all__ = [
            'Nsquared',
            'Turner_Rsubrho',
-           #'IPV_vs_fNsquared_ratio'  TODO
+           'IPV_vs_fNsquared_ratio'
            ]
 
+# FIXME: match_args_return returns a
+# ndarray instead of a tuple.
+# Need to create a test for the match_args_return.
 
-#@match_args_return FIXME: Create a test for the match_args_return.
+
+#@match_args_return
 def Nsquared(SA, CT, p, lat=None):
     r"""Calculates the buoyancy frequency squared (N^2)(i.e. the Brunt-Väisälä
     frequency squared) at the mid pressure from the equation,
@@ -104,7 +108,7 @@ def Nsquared(SA, CT, p, lat=None):
     return N2, p_mid
 
 
-@match_args_return
+#@match_args_return
 def Turner_Rsubrho(SA, CT, p):
     r"""Calculates the Turner angle and the Rsubrho as a function of pressure
     down a vertical water column.  These quantities express the relative
@@ -206,3 +210,130 @@ def Turner_Rsubrho(SA, CT, p):
     Rsubrho[Inz] = (alpha[Inz] * dCT[Inz]) / (beta[Inz] * dSA[Inz])
 
     return Tu, Rsubrho, p_mid
+
+
+#@match_args_return
+def IPV_vs_fNsquared_ratio(SA, CT, p, p_ref=0):
+    r"""Calculates the ratio of the vertical gradient of potential density to
+    the vertical gradient of locally-referenced potential density.  This
+    ratio is also the ratio of the planetary Isopycnal Potential Vorticity
+    (IPV) to f times N^2, hence the name for this variable,
+    IPV_vs_fNsquared_ratio (see Eqn. (3.20.5) of IOC et al. (2010)).  The
+    reference sea pressure, p_ref, of the potential density surface must
+    have a constant value.
+
+    IPV_vs_fNsquared_ratio is evaluated at the mid pressure between the
+    individual data points in the vertical.  This function uses the
+    computationally-efficient 48-term expression for density in terms of
+    SA, CT and p (McDougall et al., 2011).
+
+    Parameters
+    ----------
+    SA : array_like
+         Absolute Salinity  [g/kg]
+    CT : array_like
+         Conservative Temperature [deg C]
+    p : array_like
+        sea pressure  [dbar]
+    p_ref : int, float, optional
+         reference pressure, default = 0
+
+    Returns
+    -------
+    IPV_vs_fNsquared_ratio : array_like
+         The ratio of the vertical gradient of potential density,
+         on the same (M-1)xN grid as p_mid. [unitless]
+         referenced to p_ref, to the vertical gradient of locally-
+         referenced potential density.
+
+    p_mid : array_like
+            Mid pressure between p grid [dbar]
+
+    See Also
+    --------
+    TODO
+
+    Notes
+    -----
+    The 48-term equation has been fitted in a restricted range of parameter
+    space, and is most accurate inside the "oceanographic funnel" described in
+    McDougall et al. (2011).  The GSW library function "infunnel(SA, CT, p)" is
+    available to be used if one wants to test if some of one's data lies
+    outside this "funnel".
+
+    Examples
+    --------
+    TODO
+
+    References
+    ----------
+    .. [1] IOC, SCOR and IAPSO, 2010: The international thermodynamic equation
+    of seawater - 2010: Calculation and use of thermodynamic properties.
+    Intergovernmental Oceanographic Commission, Manuals and Guides No. 56,
+    UNESCO (English), 196 pp. See Eqn. (3.20.5).
+
+    ..[2] McDougall T.J., P.M. Barker, R. Feistel and D.R. Jackett, 2011:  A
+    computationally efficient 48-term expression for the density of
+    seawater in terms of Conservative Temperature, and related properties
+    of seawater.
+
+    Modifications:
+    2011-03-23. Trevor McDougall & Paul Barker
+    """
+    p_ref = np.unique(np.asanyarray(p_ref))
+
+    # BUG
+    #if not np.isscalar(p_ref):
+        #raise ValueError('The reference pressure p_ref must be unique')
+
+    if SA.ndim == 1:
+        raise ValueError('There must be at least 2 columns.')
+
+    SA.clip(0, np.inf)
+
+    SA, CT, p, p_ref = np.broadcast_arrays(SA, CT, p, p_ref)
+
+    p_ref = p_ref[:-1, ...]
+
+    p_mid = 0.5 * (p[0:-1, ...] + p[1:, ...])
+    SA_mid = 0.5 * (SA[0:-1, ...] + SA[1:, ...])
+    CT_mid = 0.5 * (CT[0:-1, ...] + CT[1:, ...])
+
+    dSA = SA[0:-1, ...] - SA[1:, ...]
+    dCT = CT[0:-1, ...] - CT[1:, ...]
+
+    [dummy, alpha, beta] = rho_alpha_beta(SA_mid, CT_mid, p_mid)
+
+    _, alpha, beta = rho_alpha_beta(SA_mid, CT_mid, p_mid)
+    _, alpha_pref, beta_pref = rho_alpha_beta(SA_mid, CT_mid, p_ref)
+
+    """This function calculates IPV_vs_fNsquared_ratio using the
+    computationally efficient 48-term expression for density in terms of SA,
+    CT and p.  If one wanted to compute this with the full TEOS-10 Gibbs
+    function expression for density, the following lines of code will enable
+    this.
+
+    pt_mid = pt_from_CT(SA_mid, CT_mid)
+    pr0 = np.zeros_like(SA_mid)
+    t_mid = pt_from_t(SA_mid, pt_mid, pr0, p_mid)
+    beta = beta_const_CT_t_exact(SA_mid, t_mid, p_mid)
+    alpha = alpha_wrt_CT_t_exact(SA_mid, t_mid, p_mid)
+    beta_pref = beta_const_CT_t_exact(SA_mid, t_mid, p_ref)
+    alpha_pref = alpha_wrt_CT_t_exact(SA_mid, t_mid, p_ref)
+    """
+
+    numerator = dCT * alpha_pref - dSA * beta_pref
+    denominator = dCT * alpha - dSA * beta
+
+    """IPV_vs_fNsquared_ratio = np.zeros_like(SA_mid) * np.NaN
+    I = denominator != 0.
+    IPV_vs_fNsquared_ratio[I] = numerator[I] / denominator[I]"""
+
+    IPV_vs_fNsquared_ratio = numerator / denominator
+
+    return IPV_vs_fNsquared_ratio, p_mid
+
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
