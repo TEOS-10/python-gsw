aa76e38b7f9ac39ae12454fff9d38a5a12d7cca1
Eric Firing <efiring@hawaii.edu>
2013-06-29 15:26:29 -1000

    mat2npz for V7 or earlier mat files, as found in 3.03

--- gsw/utilities/mat2npz.py
+++ gsw/utilities/mat2npz.py
@@ -13,15 +13,11 @@
 # obs:
 #
 
-import h5py
 import numpy as np
-
-
-def get_data(data):
-    return np.atleast_1d(np.squeeze(data)).T
+from pycurrents.file.matfile import loadmatbunch
 
 data_ver = 'v3_0'
-gsw_data = h5py.File('gsw_data_%s.mat' % data_ver, mode='r')
+gsw_data = loadmatbunch('gsw_data_%s.mat' % data_ver, masked=False)
 
 # Delta SA Atlas.
 ref_table = dict()
@@ -29,45 +25,25 @@ for k in gsw_data:
     if k == u'gsw_cv' or k == u'#refs#' or k == 'gsw_demo_data':
         pass
     else:
-        if k == 'deltaSA_ref':
-            name = 'delta_SA_ref'
-        else:
-            name = k
-        if name == 'version_number' or name == 'version_date':
-            var = ''.join([unichr(c) for c in gsw_data[k][:]])
-        else:
-            var = get_data(gsw_data[k][:])
-        ref_table.update({name: var})
+        ref_table[k] = gsw_data[k]
 np.savez("gsw_data_%s" % data_ver, **ref_table)
 
-# Save demo data values gsw_demo_data` in a separate file.
+# Save demo data values gsw_demo_data in a separate file.
 gsw_demo_data = gsw_data['gsw_demo_data']
 
-demo_vars = dict()
-for name in gsw_demo_data:
-    var = get_data(gsw_demo_data[name][:])
-    demo_vars.update({name: var})
-np.savez("gsw_demo_data_%s" % data_ver, **demo_vars)
+np.savez("gsw_demo_data_%s" % data_ver, **gsw_data['gsw_demo_data'])
 
 # Save compare values `gsw_cv` in a separate file.
-gsw_cv = gsw_data['gsw_cv']
+cv_vars = gsw_data['gsw_cv']
 
-cv_vars = dict()
-for name in gsw_cv:
-    var = get_data(gsw_cv[name][:])
-    cv_vars.update({name: var})
 np.savez("gsw_cv_%s" % data_ver, **cv_vars)
-
-gsw_data.close()
 
 # NOTE: This is a saved result of a modified version of `gsw_check_functions.m`
 # where the structure variable gsw_cf was saved.  The matlab version relies
 # on the result of some of its functions to test others, so we need this file.
-gsw_cf = h5py.File('gsw_cf.mat', mode='r')
-gsw_cf['gsw_cf']
 
-cf_vars = dict()
-for name in gsw_cf:
-    var = get_data(gsw_cf[name][:])
-    cf_vars.update({name: var})
-np.savez("gsw_cf", **cf_vars)
+# Turned off; I don't have the mat file.  Maybe the existing npz file is OK.
+if False:
+    gsw_cf = loadmatbunch('gsw_cf.mat', masked=False)
+    np.savez("gsw_cf", **gsw_cf)
+
