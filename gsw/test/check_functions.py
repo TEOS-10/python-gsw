6c8db009af3392162fd0112208131e09c47d6b5a
Eric Firing <efiring@hawaii.edu>
2013-07-04 13:17:44 -1000

    test/check_functions.py: generate, run, summarize from gsw_check_functions.m
    I am running this in ipython to see what works and what doesn't.
    Because it is generating tests by parsing gsw_check_functions,
    it doesn't require extra work when a function is added.  In
    case a test fails, it facilitates comparing the function output
    to the target output.

---
+++ gsw/test/check_functions.py
@@ -0,0 +1,166 @@
+import sys
+import os
+import logging
+
+import numpy as np
+
+from pycurrents.system import Bunch
+
+import gsw
+from gsw.gibbs import *
+
+log = logging.getLogger()
+logging.basicConfig()
+
+try:
+    mfiledir = sys.argv[1]
+except IndexError:
+    mfiledir = "../../../../TEOS-10/matlab_gsw_V3_03"
+
+mfile = os.path.join(mfiledir, "gsw_check_functions.m")
+mfilelines = open(mfile, 'rt').readlines()
+
+def find(x):
+    """
+    Numpy equivalent to Matlab find.
+    """
+    return np.nonzero(x.flatten())[0]
+
+
+first_pass = []
+
+concat = False
+for line in mfilelines:
+    line = line.strip()
+    if concat:
+        if line.endswith('...'):
+            line = line[:-3]
+        first_pass[-1] += line
+        if line.endswith(';'):
+            concat = False
+        continue
+    if '=' in line and (line.startswith('gsw_') or line.startswith('[gsw_')):
+        if line.endswith('...'):
+            line = line[:-3]
+            concat = True
+        first_pass.append(line)
+
+second_pass = []
+
+for line in first_pass:
+    if not '(' in line:
+        continue
+    if 'which' in line:
+        continue
+    line = line.replace('gsw_', '')
+    if line.startswith('['):
+        line = line[1:].replace(']', '')
+    if line.endswith(';'):
+        line = line[:-1]
+    line = line.replace('(I)', '') # for deltaSA_atlas
+    second_pass.append(line)
+
+pairs = []
+
+for i in range(len(second_pass)):
+    if 'find(' in second_pass[i] and not 'find(' in second_pass[i-1]:
+        pairs.extend(second_pass[i-1:i+1])
+
+def group_or(line):
+    """
+    Numpy wart: using bitwise or as a fake elementwise logical or,
+    we need to add parentheses.
+    """
+    if not ('find(' in line and '|' in line):
+        return line
+    i0 = line.index('find(') + 5
+    head = line[:i0]
+    tail = line[i0:]
+    parts = tail.replace('|', ') | (')
+    new = head + '(' + parts + ')'
+    return new
+
+final = [group_or(line) for line in pairs]
+
+class FunctionCheck(object):
+    def __init__(self, linepair):
+        self.linepair = linepair
+        self.runline = linepair[0]
+        self.testline = linepair[1]
+
+        # parse the line that runs the function
+        head, tail = self.runline.split('=')
+        self.outstrings = [s.strip() for s in head.split(',')]
+        self.outstr = ','.join(self.outstrings)
+        funcstr, argpart = tail.split('(', 1)
+        self.name = funcstr.strip()
+        self.argstrings = [s.strip() for s in argpart[:-1].split(',')]
+
+        # parse the line that checks the results
+        head, tail = self.testline.split('=', 1)
+        self.resultstr = head.strip()      # cv.I*
+        head, tail = tail.split('(',1)
+        self.teststr = tail.strip()[:-1]   # argument of "find()"
+
+        # To be set when run() is successful
+        self.outlist = None
+        self.result = None   # will be a reference to the cv.I* array
+        self.passed = None   # will be set to True or False
+
+        # To be set if run() is not successful
+        self.exception = None
+
+    def __str__(self):
+        return self.runline
+
+    def run(self):
+        try:
+            exec(self.runline)
+            self.outlist = [eval(s) for s in self.outstrings]
+            exec(self.testline)
+            self.result = eval(self.resultstr)
+            self.passed = len(self.result) == 0
+
+        except Exception as e:
+            self.exception = e
+
+checks = []
+for i in range(0, len(final), 2):
+    pair = final[i:i+2]
+    checks.append(FunctionCheck(pair))
+
+
+datadir = os.path.join(os.path.dirname(gsw.utilities.__file__), 'data')
+cv = Bunch(np.load(os.path.join(datadir, 'gsw_cv_v3_0.npz')))
+cf = Bunch()
+
+for fc in checks:
+    fc.run()
+
+#failures = [k[1:] for k, v in cf.items() if k.startswith('I') and len(v) > 0]
+#passes = [k[1:] for k, v in cf.items() if k.startswith('I') and len(v) == 0]
+passes = [f.name for f in checks if f.passed]
+failures = [f.name for f in checks if f.passed is False]
+
+run_problems = [f.name for f in checks if f.exception is not None]
+
+etypes = [NameError, UnboundLocalError, TypeError, AttributeError]
+ex_dict = dict()
+for exc in etypes:
+    elist = [(f.name, f.exception) for f in checks
+                if isinstance(f.exception, exc)]
+    ex_dict[exc] = elist
+
+print "\n%s tests ran with no error and with correct output" % len(passes)
+print "%s tests had an output mismatch:" % len(failures)
+print " ", "\n  ".join(failures)
+
+print "\nExceptions were raised as follows:"
+for exc in etypes:
+    print "  ", exc.__name__
+    strings = ["     %s : %s" % e for e in ex_dict[exc]]
+    print "\n".join(strings)
+    print ""
+
+checkbunch = Bunch([(c.name, c) for c in checks])
+
