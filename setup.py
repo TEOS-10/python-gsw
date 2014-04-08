8646d0c2161d36bc27e78d3726662c39e3654704
ocefpaf <ocefpaf@gmail.com>
2014-04-08 17:25:09 -0300

    Fixed setup.py for py3k.

--- setup.py
+++ setup.py
@@ -3,45 +3,63 @@
 
 from __future__ import absolute_import
 
-import codecs
+import io
+import re
 from setuptools import setup
 
-from gsw import __version__, __authors__, __email__, __maintainer__
+
+def read(*filenames, **kwargs):
+    encoding = kwargs.get('encoding', 'utf-8')
+    sep = kwargs.get('sep', '\n')
+    buf = []
+    for filename in filenames:
+        with io.open(filename, encoding=encoding) as f:
+            buf.append(f.read())
+    return sep.join(buf)
+
+
+VERSIONFILE = "gsw/__init__.py"
+verstrline = open(VERSIONFILE, "rt").read()
+VSRE = r"^__version__ = ['\"]([^'\"]*)['\"]"
+mo = re.search(VSRE, verstrline, re.M)
+if mo:
+    verstr = mo.group(1)
+else:
+    raise RuntimeError("Unable to find version string in %s." % (VERSIONFILE,))
+
+email = "ocefpaf@gmail.com"
+maintainer = "Filipe Fernandes"
+authors = ['Eric Firing', u'Bjørn Ådlandsvik', 'Filipe Fernandes']
 
 install_requires = ['numpy', 'nose']
 
-classifiers = """\
-Development Status :: 4 - Beta
-Environment :: Console
-Intended Audience :: Science/Research
-Intended Audience :: Developers
-Intended Audience :: Education
-License :: OSI Approved :: MIT License
-Operating System :: OS Independent
-Programming Language :: Python
-Topic :: Scientific/Engineering
-Topic :: Education
-Topic :: Software Development :: Libraries :: Python Modules
-"""
-
-CHANGES = open('CHANGES.txt').read()
-LICENSE = open('LICENSE.txt').read()
-README = codecs.open('README.md', encoding='utf-8').read()
+LICENSE = read('LICENSE.txt')
+long_description = read('README.txt', 'CHANGES.txt')
 
 config = dict(name='gsw',
-              version=__version__,
+              version=verstr,
               packages=['gsw', 'gsw/gibbs', 'gsw/utilities', 'gsw/test'],
               package_data={'gsw': ['utilities/data/*.npz']},
               test_suite='tests',
               use_2to3=True,
               license=LICENSE,
-              long_description=u'%s\n\n%s' % (README, CHANGES),
-              classifiers=filter(None, classifiers.split("\n")),
+              long_description=long_description,
+              classifiers=['Development Status :: 4 - Beta',
+                           'Environment :: Console',
+                           'Intended Audience :: Science/Research',
+                           'Intended Audience :: Developers',
+                           'Intended Audience :: Education',
+                           'License :: OSI Approved :: MIT License',
+                           'Operating System :: OS Independent',
+                           'Programming Language :: Python',
+                           'Topic :: Education',
+                           'Topic :: Scientific/Engineering',
+                           ],
               description='Gibbs SeaWater Oceanographic Package of TEOS-10',
-              author=__authors__,
-              author_email=__email__,
-              maintainer=__maintainer__,
-              maintainer_email=__email__,
+              author=authors,
+              author_email=email,
+              maintainer=maintainer,
+              maintainer_email=email,
               url='http://pypi.python.org/pypi/seawater/',
               download_url='https://pypi.python.org/pypi/gsw/',
               platforms='any',
