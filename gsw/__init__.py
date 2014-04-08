ebdc7bf357cd6eabda8216d2b4f13ea18e3c8eab
ocefpaf <ocefpaf@gmail.com>
2014-04-08 17:25:29 -0300

    Folded some metadata inside setup.py.

--- gsw/__init__.py
+++ gsw/__init__.py
@@ -6,14 +6,7 @@ from .utilities import (Bunch, match_args_return, read_data, strip_mask,
 
 data = read_data("gsw_data_v3_0.npz")
 
-__authors__ = ['Eric Firing', u'Bjørn Ådlandsvik', 'Filipe Fernandes']
-__license__ = "MIT"
 __version__ = "3.0.3"
-__data_version__ = str(data['version_number'])
-__data_date__ = str(data['version_date'])
-__maintainer__ = "Filipe Fernandes"
-__email__ = "ocefpaf@gmail.com"
-__status__ = "Production"
-__created_ = "14-Jan-2010"
-__modified__ = "19-August-2013"
 __all__ = ['gibbs', 'utilities']
+__data_date__ = str(data['version_date'])
+__data_version__ = str(data['version_number'])
